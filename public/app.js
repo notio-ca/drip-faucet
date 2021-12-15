429488c8b38fe8309920d78162eca3afc236e834
Luc Charest <luc@notio.ca>
2021-12-15 12:59:50 -0500

    Analytics v2

--- public/app.js
+++ public/app.js
@@ -314,3 +314,16 @@ $(".btn").mouseup(function(){
 }).mouseleave(function(){
   $(this).blur();
 });
+
+// ----------------------------------------------------------------------------------------
+// -- ANALYTICS ---------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', "UA-24554015-2"]);
+_gaq.push(['_trackPageview']);
+(function () {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+function ClickTrack(inAction, inLabel) { _gaq.push(['_trackEvent', 'Click', inAction, inLabel]); };
