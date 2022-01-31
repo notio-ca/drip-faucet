054bf952a193ac93dc9c71bab8a9f1fa7cc17d7d
Luc Charest <luc@notio.ca>
2022-01-31 15:10:58 -0500

    function

--- netlify/functions/drip-price.js
+++ netlify/functions/drip-price.js
@@ -5,8 +5,9 @@ const API_ENDPOINT = 'https://api.drip.community/prices/'
 exports.handler = async (event, context) => {
   let res, data
   try {
-    res = await fetch(API_ENDPOINT)
+    res = await fetch(API_ENDPOINT);
     data = await res.json();
+    data = { price: data[data.length-1].value };
     
   } catch (err) {
     return {
