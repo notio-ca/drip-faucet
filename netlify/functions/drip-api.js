eb239e488206f4905c02ae711303699683e1a000
Luc Charest <luc@notio.ca>
2022-02-01 17:50:29 -0500

    drip-api

--- netlify/functions/drip-api.js
+++ netlify/functions/drip-api.js
@@ -1,15 +1,15 @@
 const fetch = require('node-fetch')
 
-const API_ENDPOINT = 'http://crypto.xqub.ca/drip-api/0x287C7d1638E5771947BcdCBd8b174fAc4cF37E08'
+const API_ENDPOINT = 'http://crypto.xqub.ca/drip-api/'
 
 exports.handler = async (event, context) => {
   let res, data
   try {
-    res = await fetch(API_ENDPOINT);
-    data_api = await res.json();
     param = event.path.split("/");
     param = param[param.length-1];
-    data = { data_api:data_api, url:param };
+    res = await fetch(API_ENDPOINT + param);
+    data_api = await res.json();
+    data = data_api;
     
   } catch (err) {
     return {
