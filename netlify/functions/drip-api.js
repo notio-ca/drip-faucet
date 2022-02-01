dd61faac2baaa53692441be1967f3aac1947bdfd
Luc Charest <luc@notio.ca>
2022-02-01 17:46:04 -0500

    drip-api

--- netlify/functions/drip-api.js
+++ netlify/functions/drip-api.js
@@ -6,8 +6,8 @@ exports.handler = async (event, context) => {
   let res, data
   try {
     res = await fetch(API_ENDPOINT);
-    data = await res.json();
-    //data = { symbol:"drip", price: Math.round(data[data.length-1].value * 100) / 100 };
+    data_api = await res.json();
+    data = { data_api:data, url:event.path };
     
   } catch (err) {
     return {
