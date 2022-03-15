26ed064a3f849175299c8ae4b4aeabf5556b448c
Luc Charest <luc@notio.ca>
2022-03-15 10:15:35 -0400

    update

--- public/web3modal/example.js
+++ public/web3modal/example.js
@@ -51,15 +51,15 @@ function init() {
         // Mikko's test key - don't copy as your mileage may vary
         //infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
       }
-    },
-
+    }
+    
+    /*,
     fortmatic: {
       package: Fortmatic,
       options: {
-        // Mikko's TESTNET api key
         key: "pk_test_391E26A3B43A3350"
       }
-    }
+    }*/
   };
 
   web3Modal = new Web3Modal({
